22c98fd1d5fd220a4f94145c26649e618c8d9f49
mabelxue <xueym0106@icloud.com>
2017-04-12 15:01:17 +0800

    使用自定义布局显示ListView。

---
+++ app/src/main/java/com/firstline/listviewtest/FruitAdapter.java
@@ -0,0 +1,58 @@
+package com.firstline.listviewtest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+
+    private int resourceId;
+
+    /**
+     * FruitAdapter的构造函数
+     * @param context 上下文
+     * @param textViewResourceId 是ListView子项局部的id
+     * @param objects 数据
+     */
+    public FruitAdapter(Context context, int textViewResourceId,
+                        List<Fruit> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    // 在每个子项被滚动到屏幕内的时候调用
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Fruit fruit = getItem(position); // 获取当前项的Fruit实例
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById (R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById (R.id.fruit_name);
+            view.setTag(viewHolder); // 将ViewHolder存储在View中
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag(); // 重新获取ViewHolder
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
+        return view;
+    }
+
+    class ViewHolder {
+
+        ImageView fruitImage;
+
+        TextView fruitName;
+
+    }
+
+}
